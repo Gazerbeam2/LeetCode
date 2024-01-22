dea3b3f358711875a6e44229da78b9b81ae495e8
Gazerbeam <134249845+Gazerbeam2@users.noreply.github.com>
2024-01-21 19:50:04 -0600

    2703-Return-Length-of-Arguments-Passed

---
+++ javascript_solutions/2703-Return-Length-of-Arguments-Passed/2703-Return-Legth-of-Arguments-Passed.js
@@ -0,0 +1,8 @@
+var argumentsLength = function(...args){
+    return args.length
+};
+
+
+//We need to know how many arguments have been passed into the function.
+
+//Returning the length method will tell you how many arguments will be passed into it.
